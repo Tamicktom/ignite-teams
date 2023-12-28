ec3e1ba7d0f14bf414d4c8f979febc1280b7cc54
Henrique Fonseca <rickpokemon38@gmail.com>
2023-12-28 18:49:36 -0300

    Update styled-components import in App.tsx

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 //* Libraries imports
 import { StatusBar } from "react-native";
-import { ThemeProvider } from "styled-components";
+import { ThemeProvider } from "styled-components/native";
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from "@expo-google-fonts/roboto";
 
 //* Components imports
